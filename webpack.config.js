491c203f1742fff6457d1107761cb0ed2f8b77aa
douglasroberto1997 <douglasroberto47@gmail.com>
2025-05-21 16:01:10 -0300

    adding copy webpacl plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require ("path")
 const htmlWepackPlugin = require ("html-webpack-plugin")
+const CopyWebpackPlugin = require ("copy-webpack-plugin")
+
 module.exports = {
     target: "web",
     mode: "development",
@@ -21,6 +23,14 @@ module.exports = {
     plugins: [new htmlWepackPlugin ({
         template: path.resolve(__dirname, "index.html"),
         favicon: path.resolve ("src", "assets", "scissors.svg"),
+    }),
+    new CopyWebpackPlugin({
+        patterns: [
+            {
+            from: path.resolve(__dirname, "src", "assets"),
+            to: path.resolve (__dirname, "dist", "src", "assets")
+            }
+        ],
     })
     ],
 
