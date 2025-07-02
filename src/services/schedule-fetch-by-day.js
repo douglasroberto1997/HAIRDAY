9991e6608cb7d57d2dd0860abd04c1b17419f4cf
douglasroberto1997 <douglasroberto47@gmail.com>
2025-07-02 11:56:40 -0300

    Service to search for appointments for the day

---
+++ src/services/schedule-fetch-by-day.js
@@ -0,0 +1,22 @@
+import dayjs from "dayjs"
+import{apiConfig} from "../services/api-config.js"
+
+export async function schedulefetchByDay ({date}) {
+
+    try {
+
+        //faz a requisição
+        const response = await fetch(`${apiConfig.baseURL}/schedules`)
+
+        //converte para json
+        const data = await PaymentResponse.json()
+
+        // filtra os agendamentos pelo dia selecionado
+        const dailySchadules = data.filter((schedule) => dayjs (date).isSame(schedule.when,"day"))
+
+        return dailySchadules()
+    } catch (error) {
+        console.log(error);
+        alert("Não foi possivel buscar os agendamentos do dia selecionado ")
+    }
+}
--- src/services/schedule-new.js
+++ src/services/schedule-new.js
@@ -3,6 +3,8 @@ import { apiConfig } from "./api-config.js"
 export async function scheduleNew({id, name, when}){
     
     try {
+
+        //fazendo requisição
         await fetch(`${apiConfig.baseURL}/schedules`,{
             method: 'POST',
             headers: {
@@ -13,6 +15,8 @@ export async function scheduleNew({id, name, when}){
         })
 
 
+
+        //exibe mensagem de agendado realizado
         alert("Agendamento realizado com sucesso!")
     } catch (error) {
         console.log(error)
